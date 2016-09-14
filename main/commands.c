6657908a4eec37071b93c520d9c72b1e68b56fb5
jubujjamer <jubujjamer@yahoo.com.ar>
2016-09-14 18:20:03 -0300

    Some functions tested

--- main/commands.c
+++ main/commands.c
@@ -10,34 +10,35 @@ int main(int argc, char *argv[]){
 	int opt = 0;
 	char *in_fname = NULL;
 	char *out_fname = NULL;
+	FILE *fp;
 
-	while ((opt = getopt(argc, argv, "newday:o:")) != -1) {
+	while ((opt = getopt(argc, argv, "n:o:")) != -1) {
 		switch(opt) {
-	  	case 'i':
-	    	in_fname = optarg;
-	    	printf("\nInput option value=%s", in_fname);
-	    	break;
-	    case 'o':
-	    	out_fname = optarg;
-	    	printf("\nOutput option value=%s", out_fname);
-	    	break;
-	    case '?':
-	    	/* Case when user enters the command as
-	     	* $ ./cmd_exe -i
-	     	*/
-	    if (optopt == 'i') {
-	    	printf("\nMissing mandatory input option");
-	    	/* Case when user enters the command as
-	     	* # ./cmd_exe -o
-	     	*/
-	  	}
-			else if (optopt == 'o') {
-	    	printf("\nMissing mandatory output option");
-	  	}
-			else {
-	    	printf("\nInvalid option received");
-	  	}
-	  	break;
+			case 'n':
+	    	printf("\nNo arguments on this");
+	    		break;
+	    		case 'o':
+	    			out_fname = optarg;
+	    			printf("\nOutput option value=%s", out_fname);
+	    			break;
+	    		case '?':
+	    			/* Case when user enters the command as
+	     			* $ ./cmd_exe -i
+	     			*/
+	    		if (optopt == 'o') {
+	    			printf("\nMissing mandatory output option");
+	  			}
+					else if (optopt == 'n') {
+	    			printf("\nCreating a new file");
+						mkdir("day folder",0700);
+						fp = fopen("day.adoc","w");
+						fprintf(fp, "Hello\n");
+						fclose(fp);
+	  			}
+					else {
+	    			printf("\nInvalid option received");
+	  			}
+	  			break;
 	 		}
 		}
 		printf("\n");
