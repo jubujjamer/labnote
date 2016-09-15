a305fd068cc77a98ec3c3eb9662fdcac8a3b1fab
jubujjamer <jubujjamer@yahoo.com.ar>
2016-09-15 18:23:27 -0300

    names.h file added with common names. commands file with newfile functionality

---
+++ main/names.h
@@ -0,0 +1,7 @@
+// Define directory names
+#define  WORKING_PATH  "my_labnote_dir"
+#define HOME "/home/lec"
+#define  CONTENT_DIR   "content"
+#define  DOCS_DIR      "documents"
+#define  INVENTORY_DIR "inventory"
+#define  GENERAL_DIR "general"
